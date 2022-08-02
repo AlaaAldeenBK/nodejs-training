93c3e80e57538874e943c2941d4a14b89b204507
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-08-02 15:29:54 +0300

    Js file

---
+++ index.js
@@ -0,0 +1,20 @@
+const server = require('http');
+const urlModule = require('url');
+const serverApp = server.createServer((req, res) =>{
+    res.setHeader("Content-Type","text/html");
+    res.statusCode = 404;
+    // res.write(req.url);
+    // var link = req.url;
+    // console.log(link);
+    // var linkParts = req.url.split("/");
+    // console.log(linkParts);
+
+    const urlObj = urlModule.parse("http://127.0.0.1:3002/login/user?id=4&type=customer",true);
+    const queryItem = urlObj.query;
+    res.write(queryItem.id);
+    // res.write(req.url);
+    // res.write(linkParts[3]);
+    res.end();
+});
+serverApp.listen("3003");
+console.log("server start");
