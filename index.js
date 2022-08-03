411da1ac9a37ac886959fe2d27160cfcb2cdf742
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-08-03 13:53:35 +0300

    Node file

--- index.js
+++ index.js
@@ -10,8 +10,27 @@ const serverApp = server.createServer((req, res) =>{
     // console.log(linkParts);
 
     const urlObj = urlModule.parse("http://127.0.0.1:3002/login/user?id=4&type=customer",true);
-    const queryItem = urlObj.query;
-    res.write(queryItem.id);
+    if(urlObj.hostname =="/")
+    {
+        res.write("Welcome to home page");
+        res.end();
+    }
+    else if(urlObj.hostname == "/Product")
+    {
+        res.write("Welcome to home page");
+        res.end();
+    }
+    else if(urlObj.hostname == "login")
+    {
+        if(req.method == "POST")
+        {
+            res.write("Welcome to home page");
+            res.end();
+        }
+        
+    }
+    // const queryItem = urlObj.query;
+    // res.write(queryItem.id);
     // res.write(req.url);
     // res.write(linkParts[3]);
     res.end();
